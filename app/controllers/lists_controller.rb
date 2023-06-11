f93d16a32de87b8122c95e5c8d61234b85cb12d6
EC2 Default User <ec2-user@ip-172-31-36-88.ap-northeast-1.compute.internal>
2023-06-11 14:48:39 +0000

    [Finish]4-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,27 +2,42 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
     redirect_to list_path(list.id)
   end
-  
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
   def index
     @lists = List.all
-  end 
+  end
 
   def show
     @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
-  
-  private 
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+
+  private
+
   def list_params
     #データベースに保存する内容を決める
-    params.require(:list).permit(:title, :body)
-  end 
+    params.require(:list).permit(:title, :body, :image)
+  end
 end
